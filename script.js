76e696163ec1b008143566aa67719a3352930c8c
MunnyReol <59450380+MunnyReol@users.noreply.github.com>
2020-03-23 16:52:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document ).ready(function (){
+var id= prompt("enter your favorite youtuber channel id", "UCq-Fj5jknLsUf-MWSy4_brA")
+$("button").click(function (){
+setInterval(function() {
+$.getJSON("https://cors.livecounts.io/https://freewebtools.com/compteur-youtube/?channelId="+id, function (subss) {
+
+        sub = subss.subscriberCount;
+        view=subss.viewCount;
+        title=subss.title;
+        pic=subss.picture;
+        
+        $("#view").text(view)
+        $(".title").text(title);
+        $("#subs").text(sub);
+        
+        console.log("sub="+sub+"\n","view="+view)
+        
+         var image = document.querySelector('#user_pic'); image.src = pic;
+         
+        })
+        }, 1000); 
+    });
+});
